7813f589dbdb2fb8dda431ce2fdd177847fd2626
Concision <contact@concision.me>
2019-10-09 17:25:23 -0700

    Added enums for source, destination, and format

---
+++ src/main/java/me/concision/warframe/decacher/output/OutputDestination.java
@@ -0,0 +1,11 @@
+package me.concision.warframe.decacher.output;
+
+/**
+ * @author Concision
+ * @date 10/9/2019
+ */
+public enum OutputDestination {
+    DIRECTORY,
+    FILE,
+    STANDARD_OUT
+}
---
+++ src/main/java/me/concision/warframe/decacher/source/PackagesSource.java
@@ -0,0 +1,11 @@
+package me.concision.warframe.decacher.source;
+
+/**
+ * @author Concision
+ * @date 10/7/2019
+ */
+public enum PackagesSource {
+    ORIGIN_SERVER,
+    LOCAL_INSTALLATION,
+    LOCAL_BINARY
+}
