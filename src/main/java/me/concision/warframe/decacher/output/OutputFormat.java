b2fbc259d8f0b083326411f78acf4b6e2f00a229
Concision <contact@concision.me>
2019-10-09 17:25:23 -0700

    Added enums for source, destination, and format

---
+++ src/main/java/me/concision/warframe/decacher/output/OutputFormat.java
@@ -0,0 +1,12 @@
+package me.concision.warframe.decacher.output;
+
+/**
+ * @author Concision
+ * @date 10/9/2019
+ */
+public enum OutputFormat {
+    PATHS,
+    RECORDS,
+    RECURSIVE,
+    FLATTENED
+}
