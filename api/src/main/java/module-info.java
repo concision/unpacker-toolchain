bdaa6e72607d1648f5f8814f7f3034ff680b96e7
Concision <contact@concision.me>
2020-08-29 01:17:56 -0700

    fix(api): Removed module-info dependency on codec
    Signed-off-by: Concision <contact@concision.me>

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module me.concision.unnamed.packages.api {
     requires static lombok;
     requires org.mongodb.bson;
-    requires org.apache.commons.codec;
 
     exports me.concision.unnamed.decacher.api;
     exports me.concision.unnamed.unpacker.api;
