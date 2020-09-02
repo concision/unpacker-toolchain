0320bec3ecfee54b2a09fefed7eadf9cd1d84d73
XanZath <xanzath@gmail.com>
2020-09-02 21:56:12 +0200

    feat(server/docker): Configured development docker-compose
    Took 1 hour 29 minutes

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 # run packages api server
-uvicorn src.main:app --host 0.0.0.0 --port 80
+# shellcheck disable=SC2039
+uvicorn src.main:app \
+  --host 0.0.0.0 \
+  --port 80 \
+  "$([ "${DEVELOPMENT}" == "true" ] && echo --reload)"
