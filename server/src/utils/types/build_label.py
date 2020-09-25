ba657ffeeb448b343e7d049afcd4cbf5aa122a1c
osego <60160526+osego@users.noreply.github.com>
2020-09-25 22:50:47 +0200

    feat(server): renamed compile_time to build_date in BuildLabel, hash part is now optional

--- server/src/utils/types/build_label.py
+++ server/src/utils/types/build_label.py
@@ -1,11 +1,17 @@
+from typing import Optional
+
+
 class BuildLabel:
     raw: str
-    compile_time: str
-    build_hash: str
+    build_date: str
+    build_hash: Optional[str]
 
     def __init__(self, build_label: str):
         self.raw = build_label
-        self.compile_time, self.build_hash = self.raw.split('/')
+        if '/' in build_label:
+            self.build_date, self.build_hash = self.raw.split('/')
+        else:
+            self.build_date = build_label
 
     def __str__(self):
         return self.raw
