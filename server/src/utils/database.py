05cca3d43d81371c80747aa3834d7bf2005b89dd
osego <60160526+osego@users.noreply.github.com>
2020-09-26 04:53:57 +0200

    feat(server): reworked data insertion database access method
    fixed runtime error in environment with empty database

--- server/src/utils/database.py
+++ server/src/utils/database.py
@@ -6,8 +6,10 @@ from hashlib import sha256
 from datetime import datetime
 
 import asyncpg
+from fastapi import UploadFile, Request
 
 from .types import UpdateInfo
+from .unpacker import Unpacker
 
 
 def wait_until_ready(check):
@@ -48,32 +50,35 @@ class Database:
     @wait_until_ready("conn")
     async def latest_version(self):
         data = await self.conn.fetchrow(
-            "SELECT build_label FROM package_labels ORDER BY build_version_ordinal DESC LIMIT 1")
-        return data["build_label"] if "build_label" in data else None
+            "SELECT build_label FROM package_labels ORDER BY build_date_ordinal DESC LIMIT 1")
+        return data["build_label"] if data is not None else None
 
     @wait_until_ready("conn")
-    async def new_packages(self, records: dict, packages: bytes, update_info: UpdateInfo):
+    async def new_packages(self, packages: UploadFile, update_info: UpdateInfo):
+        build_label = str(update_info.build_label)
+        forum_version = str(update_info.semver) if update_info.semver else None
+        forum_url = update_info.forum_link
+
         async with self.conn.transaction():
             await self.conn.execute(
                 "INSERT INTO package_labels (timestamp, build_label, forum_version, forum_url) VALUES ($1, $2, $3, $4)",
-                datetime.now(), str(update_info.build_label), str(update_info.semver), update_info.forum_link
+                datetime.now(), build_label, forum_version, forum_url
             )
-            await self.conn.execute(
-                "INSERT INTO package_bins (build_version, packages) VALUES ($1, $2)",
-                update_info.build_label.compile_time, packages
-            )
-            for key, value in records:
-                _hash = sha256(bytes(value, "utf-8")).digest()
-                is_in = bool(await self.conn.execute(
-                    "SELECT COUNT(sha256) AS c FROM package_blobs WHERE sha256 = $1",
-                    _hash
-                ))
-                if not is_in:
-                    await self.conn.execute(
-                        "INSERT INTO package_blobs (sha256, contents) VALUES ($1, $2)",
-                        _hash, json.dumps(json.loads(value))
-                    )
+            try:
+                async with self.conn.transaction():
+                    with Unpacker() as unpacker:
+                        async for package_record in unpacker.unpack(source=packages):
+                            _hash = sha256(bytes(package := json.dumps(package_record["package"]), "utf-8")).digest()
+                            await self.conn.execute(
+                                "INSERT INTO package_blobs (sha256, contents) VALUES ($1, $2) ON CONFLICT DO NOTHING",
+                                _hash, package
+                            )
+                            await self.conn.execute(
+                                "INSERT INTO package_entries (build_date, path, sha256) VALUES ($1, $2, $3)",
+                                update_info.build_label.build_date, package_record["path"], _hash
+                            )
+            finally:
                 await self.conn.execute(
-                    "INSERT INTO package_entries (build_version, path, sha256) VALUES ($1, $2, $3)",
-                    update_info.build_label.compile_time, key, _hash
+                    "INSERT INTO package_bins (build_date, packages) VALUES ($1, $2)",
+                    update_info.build_label.build_date, unpacker.compressed_packages.getvalue()
                 )
