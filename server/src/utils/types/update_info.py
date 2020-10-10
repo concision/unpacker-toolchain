08a52e271c376723036447e41864809a03e642d8
osego <60160526+osego@users.noreply.github.com>
2020-10-10 23:41:11 +0200

    feat(server): more elaborate forum_link type

--- server/src/utils/types/update_info.py
+++ server/src/utils/types/update_info.py
@@ -1,5 +1,7 @@
 from typing import Union, Optional
 
+from pydantic import HttpUrl
+
 from .semver import SemVer
 from .build_label import BuildLabel
 
@@ -7,9 +9,9 @@ from .build_label import BuildLabel
 class UpdateInfo:
     semver: Union[SemVer, type(None)]
     build_label: BuildLabel
-    forum_link: Union[str, type(None)]
+    forum_link: Union[HttpUrl, type(None)]
 
-    def __init__(self, build_label: BuildLabel, semver: Optional[SemVer] = None, forum_link: Optional[str] = None):
+    def __init__(self, build_label: BuildLabel, semver: Optional[SemVer] = None, forum_link: Optional[HttpUrl] = None):
         self.semver = semver
         self.build_label = build_label
         self.forum_link = forum_link
