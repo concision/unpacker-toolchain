4df301d9fbf9ead8ff4c33171f423c47e75b07fe
osego <60160526+osego@users.noreply.github.com>
2020-10-11 00:34:09 +0200

    feat(server): added UpdateIdentifier model

---
+++ server/src/utils/types/models/__init__.py
@@ -0,0 +1,5 @@
+from .update_identifier import UpdateIdentifier
+
+__all__ = (
+    "UpdateIdentifier"
+)
---
+++ server/src/utils/types/models/update_identifier.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from pydantic import BaseModel, root_validator
+
+from .. import SemVer, BuildLabel
+
+
+class UpdateIdentifier(BaseModel):
+    semver: Optional[SemVer]
+    build_label: Optional[BuildLabel]
+
+    @root_validator
+    def exactly_one_identifier(cls, values):
+        if not sum([1 if value else 0 for _, value in values]) == 1:
+            raise ValueError("Exactly one update identifier must be provided")
