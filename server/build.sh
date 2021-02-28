46040ea594f397f8bddf3fe731b019e9ea863eb7
Concision <contact@concision.me>
2021-02-28 10:13:00 -0800

    fix(server): revert an accidental deletion during branch merging
    Signed-off-by: Concision <contact@concision.me>

---
+++ server/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# Builds production server Docker image
+
+__execute_local() {
+    # short circuit if any command fails (see https://stackoverflow.com/a/4346420)
+    set -e -o pipefail
+    # see https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+    trap 'EXIT_CODE=$? && [[ ${EXIT_CODE} -ne 0 ]] && (echo "Failed to build image" && exit ${EXIT_CODE})' ERR
+
+    # script directory (see https://stackoverflow.com/a/246128)
+    local DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+    # version tag
+    local PROJECT_VERSION=1.2.0
+    echo "Project version: ${PROJECT_VERSION}"
+
+    # build project
+    echo "Building docker image"
+    docker build \
+        `# tag image (see https://stackoverflow.com/a/3545363)` \
+        --tag "concision/unpacker:${PROJECT_VERSION}" \
+        `# specify production dockerfile` \
+        --file "${DIR}/Dockerfile" \
+        `# set build target if specified` \
+        $([[ $# -ne 0  ]] && echo --target "${1}") \
+        `# normalize path to workspace root` \
+        "${DIR}/.."
+}
+
+
+# check if NOT inside a GitLab CI
+if [[ -z "${GITLAB_CI}" ]]; then
+    __execute_local "$@"
+else
+    echo Unsupported CI
+    exit 1
+fi
+
+# clean up environment
+unset __execute_local __execute_kaniko
